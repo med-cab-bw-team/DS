4faa3f25bfbbc434dfec74112a0d3a62b7005240
dustin-py <dustinstringer92@gmail.com>
2020-10-15 12:21:46 -0700

    Added module doctsting

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -1,6 +1,8 @@
+"""This file is for database managment"""
 from pymongo import MongoClient
 from os import getenv
 from dotenv import load_dotenv
+# from pydantic import BaseModel  <-- To be used for database schema set up
 
 
 class DataBase:
@@ -12,7 +14,7 @@ class DataBase:
         """Connect to MongoDB client"""
         MongoUSER = getenv('MongoUSER', default="OOPS")
         MongoPASS = getenv('MongoPASS', default="OOPS")
-        MongoNAME = getenv('MongoHOST', default="OOPS")
+        MongoNAME = getenv('MongoNAME', default="OOPS")
         client = MongoClient(
             f"mongodb+srv://{MongoUSER}:{MongoPASS}@{MongoNAME}?\
                 retryWrites=true&w=majority"
