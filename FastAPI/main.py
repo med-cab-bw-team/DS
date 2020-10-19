e8e03e728fac38dbcce0ee32eb1593a762a67a66
dustin-py <dustinstringer92@gmail.com>
2020-10-19 11:12:02 -0700

    update api home page

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
-# from fastapi.staticfiles import StaticFiles
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 api = FastAPI()
@@ -20,7 +20,7 @@ api.add_middleware(
     allow_headers=["*"]
 )
 
-# api.mount("/static", StaticFiles(directory="static"), name="static")
+api.mount("/static", StaticFiles(directory="FastAPI/static"), name="static")
 
 templates = Jinja2Templates(directory="FastAPI/templates")
 
