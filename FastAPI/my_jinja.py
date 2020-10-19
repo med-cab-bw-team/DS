44bf52248054a53e602bd0dc0777b710d81349e5
dustin-py <dustinstringer92@gmail.com>
2020-10-19 15:02:22 -0700

    added api param to mount_static_directory funtion

--- FastAPI/my_jinja.py
+++ FastAPI/my_jinja.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 
-def mount_static_directory():
+def mount_static_directory(api):
     api.mount(
         "/static",
         StaticFiles(directory="FastAPI/static"),
