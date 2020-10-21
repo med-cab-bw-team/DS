e4ff38054e6a6753340fba74524364855a38ea44
MacUser <macuser@MacUsers-MacBook-Pro.local>
2020-10-20 22:44:17 -0400

    Updated CORS Middleware origin locations

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -14,6 +14,7 @@ origins = [
 api.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_origin_regex='https?://.*',
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
